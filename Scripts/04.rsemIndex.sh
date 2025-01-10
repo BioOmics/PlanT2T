8e36b024d9d933082e89037e16c0c801be778ce3
BioOmics <90701365+BioOmics@users.noreply.github.com>
2025-01-10 16:24:55 +0800

    Update 04.rsemIndex.sh

--- Scripts/04.rsemIndex.sh
+++ Scripts/04.rsemIndex.sh
@@ -1,15 +1,15 @@
 # Usage: bash 04.rsemIndex.sh namaList
 
-# mkdir RSEMIndex
-# cd RSEMIndex
+mkdir RSEMIndex
+cd RSEMIndex
 
-# rsem-gff3-to-gtf ../genome.renamed.gff ./genome.renamed.gtf
-# rsem-prepare-reference --gtf ./genome.renamed.gtf ../genome.re.fa ./RSEMIndex -p 30 --star
+rsem-gff3-to-gtf ../genome.renamed.gff ./genome.renamed.gtf
+rsem-prepare-reference --gtf ./genome.renamed.gtf ../genome.re.fa ./RSEMIndex -p 30 --star
 
-# if [[ ! -f "./RSEMIndex.n2g.idx.fa" ]];then
-#     echo "Fail to build RSEM index"
-#     exit 1
-# fi
+if [[ ! -f "./RSEMIndex.n2g.idx.fa" ]];then
+    echo "Fail to build RSEM index"
+    exit 1
+fi
 
-echo "Skip RSEM and STAR index building"
+# echo "Skip RSEM and STAR index building"
 
