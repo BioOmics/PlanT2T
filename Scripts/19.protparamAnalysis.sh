ef9bfe684e0043796a2e4851c7593c81230680fc
BioOmics <90701365+BioOmics@users.noreply.github.com>
2025-01-10 16:29:50 +0800

    Update 19.protparamAnalysis.sh

--- Scripts/19.protparamAnalysis.sh
+++ Scripts/19.protparamAnalysis.sh
@@ -5,7 +5,7 @@ namaList=$(realpath $1)
 cat ${namaList} | cut -f1 | while read i;do
 
 cut -d " " -f1 genome.re.pep | sed '/^[^>]/ s/\.//g' | sed '/^[^>]/ s/X//g' > ${i}.pep
-python /public/workspace/biobigdata/project/Plant2t/software/script/19.protparamAnalysis.py ${i}.pep ${i}.protparam.txt
+python 19.protparamAnalysis.py ${i}.pep ${i}.protparam.txt
 
 rm ${i}.pep
 
