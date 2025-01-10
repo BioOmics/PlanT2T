73541ce2ede199b4692a84a25732ef3c0ec99480
BioOmics <90701365+BioOmics@users.noreply.github.com>
2025-01-10 16:30:09 +0800

    Update 20.SaveResultToMySQL.sh

--- Scripts/20.SaveResultToMySQL.sh
+++ Scripts/20.SaveResultToMySQL.sh
@@ -3,8 +3,8 @@
 namaList=$(realpath $1)
 
 cat ${namaList} | cut -f1 | while read i;do
-python /public/workspace/biobigdata/project/Plant2t/software/script/20.getAnotateSeq.py ${i}
-python /public/workspace/biobigdata/project/Plant2t/software/script/20.getGeneFunction.py ${i}
-python /public/workspace/biobigdata/project/Plant2t/software/script/20.getGeneTable.py ${i}
-bash /public/workspace/biobigdata/project/Plant2t/software/script/20.spe_gene_sql.sh ${i}
+python 20.getAnotateSeq.py ${i}
+python 20.getGeneFunction.py ${i}
+python 20.getGeneTable.py ${i}
+bash 20.spe_gene_sql.sh ${i}
 done
