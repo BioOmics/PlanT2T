2dba6f39d220741736589d1009bd44be1177f1a6
BioOmics <90701365+BioOmics@users.noreply.github.com>
2025-01-10 16:26:33 +0800

    Update 10.runBUSCO.sh

--- Scripts/10.runBUSCO.sh
+++ Scripts/10.runBUSCO.sh
@@ -1,5 +1,5 @@
 # Usage: bash 10.runBUSCO.sh namaList
 
 thread=20
-/public/workspace/biobigdata/project/Plant2t/software/busco-5.8.2/bin/busco -i genome.re.fa -o busco_result -m genome --offline -c ${thread} -f \
--l /public/workspace/biobigdata/project/Plant2t/software/busco-5.8.2/buscodb/embryophyta_odb10
+busco -i genome.re.fa -o busco_result -m genome --offline -c ${thread} -f \
+-l embryophyta_odb10
